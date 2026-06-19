2b26029671133f21272c78b3e8699b0b38504c7f
ehxhfl31436 <ehxhfl31436@gmail.com>
2026-06-19 17:01:30 +0900

    feat: 클래스패치와 메인이 없어 메인을 추가함

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+import controller.featureController;
+
+public class Main {
+
+	public static void main(String[] args) {
+		featureController controller = new featureController();
+		controller.run();
+
+	}
+
+}
