a3c418885281004dc39e07c8538ada2b28b049de
ehxhfl31436 <ehxhfl31436@gmail.com>
2026-06-19 17:05:47 +0900

    feat: classpath gitignore에 추가

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,7 @@ import controller.featureController;
 
 public class Main {
 
+	//
 	public static void main(String[] args) {
 		featureController controller = new featureController();
 		controller.run();
